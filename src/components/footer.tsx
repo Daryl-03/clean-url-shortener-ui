34ae1f44e0531eb025bd0f09a3921afc35045ded
Daryl <donamedenou@gmail.com>
2025-11-20 08:31:33 +0100

    fix: adjust layout in Footer component for better responsiveness

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@
 export default function Footer() {
 	return (
 		<footer className="w-full border-t-[1px] border-secondary mt-20">
-			<div className="container mx-auto py-8 px-6 md:px-40 flex justify-between items-center">
+			<div className="container mx-auto py-8 px-6 md:px-40 flex flex-col md:flex-row gap-2 md:gap-0 justify-between items-center">
 				<p className="text-muted-foreground">&copy; {new Date().getFullYear()} Hopper. All rights reserved.</p>
 				<div className="flex gap-4">
 					<a href="/privacy" className="text-muted-foreground hover:text-foreground">Privacy</a>
